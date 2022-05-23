bfafd416d3b7a987f0dc624a68bd7aae70b5cb89
Anupam-dagar <dagaranupam@gmail.com>
2022-05-23 23:33:09 +0530

    configure cors.

--- utils/express.ts
+++ utils/express.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
 import logger from "morgan";
 import router from "../routes";
+import cors from "cors";
 
 const app: Express = express();
+app.use(cors());
 app.use(logger("short"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
