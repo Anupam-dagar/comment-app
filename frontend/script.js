2811a48be534a895f23b85df49217941f5d2bf9c
Anupam-dagar <dagaranupam@gmail.com>
2022-05-24 01:54:47 +0530

    modularise frontend script.js into functions.

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,14 @@
+const backendUrl = "http://localhost";
+let user;
+
 $(document).ready(function () {
-  const backendUrl = "http://localhost";
-  let user;
+  addCommentInputListener();
+  getUser();
+  getComments();
+  createComment();
+});
+
+const getUser = () => {
   $.get(`${backendUrl}/api/users/login`, (data) => {
     $("#current-user").attr("src", data.photoUrl);
     user = {
@@ -11,68 +19,28 @@ $(document).ready(function () {
   }).fail((error) => {
     console.log(`error getting user. ${error}`);
   });
+};
 
+const getComments = () => {
   $.get(`${backendUrl}/api/comments`, (data) => {
     const comments = data.comments;
     let commentRows = "";
     comments.forEach((comment) => {
-      commentRows += `
-      <div class="d-flex flex-row align-items-start mt-5" id="${comment.id}">
-        <div>
-          <img
-            class="rounded-circle"
-            style="width: 41px"
-            src="${comment.user.photoUrl}"
-          />
-        </div>
-        <div class="flex-fill">
-          <div class="d-flex flex-row align-items-start px-2">
-            <div class="px-2">
-              <h6 class="c-text-primary">
-                ${comment.user.name}
-                <span style="font-size: 0.89em" class="c-text-secondary"
-                  >・ ${moment(comment.createdAt).fromNow()}</span
-                >
-              </h6>
-            </div>
-          </div>
-          <div class="d-flex flex-row align-items-start px-2">
-            <div class="px-2">
-              <p>
-                ${comment.comment}
-              </p>
-            </div>
-          </div>
-          <div class="d-flex flex-row align-items-start">
-            <ul class="nav">
-              <li class="nav-item">
-                <a class="nav-link c-text-secondary" href="#"
-                  ><i class="bi bi-caret-up-fill"></i> Upvote</a
-                >
-              </li>
-              <li class="nav-item">
-                <a class="nav-link c-text-secondary" href="#">Reply</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      `;
+      commentRows += getCommentRow(
+        comment.id,
+        comment.user.photoUrl,
+        comment.user.name,
+        comment.createdAt,
+        comment.comment
+      );
     });
     $("#comments").append(commentRows);
   }).fail(function (error) {
     console.log(`error fetching comments. ${error}`);
   });
+};
 
-  $("#comment").on("input", () => {
-    const input = $("#comment").val();
-    if (input.length == 0) {
-      $("#create-comment-button").attr("disabled", true);
-    } else {
-      $("#create-comment-button").attr("disabled", false);
-    }
-  });
-
+const createComment = () => {
   $("#create-comment").submit((event) => {
     event.preventDefault();
     const comment = $("#comment").val();
@@ -85,22 +53,59 @@ $(document).ready(function () {
       },
       dataType: "json",
       success: (data) => {
-        const commentRow = `
-      <div class="d-flex flex-row align-items-start mt-5" id="${data.id}">
+        const commentRow = getCommentRow(
+          data.id,
+          user.photoUrl,
+          user.name,
+          data.createdAt,
+          comment
+        );
+        $("#comments").append(commentRow);
+        $("#comment").val("");
+        $("#create-comment-button").attr("disabled", true);
+      },
+      error: (error) => {
+        console.log(`error creating comment. ${error}`);
+        alert("Error making comment. Please try again in a while.");
+      },
+    });
+  });
+};
+
+const addCommentInputListener = () => {
+  $("#comment").on("input", () => {
+    const input = $("#comment").val();
+    if (input.length == 0) {
+      $("#create-comment-button").attr("disabled", true);
+    } else {
+      $("#create-comment-button").attr("disabled", false);
+    }
+  });
+};
+
+const getCommentRow = (
+  commentId,
+  userPhotoUrl,
+  userName,
+  commentCreatedAt,
+  comment
+) => {
+  return `
+      <div class="d-flex flex-row align-items-start mt-5" id="${commentId}">
         <div>
           <img
             class="rounded-circle"
             style="width: 41px"
-            src="${user.photoUrl}"
+            src="${userPhotoUrl}"
           />
         </div>
         <div class="flex-fill">
           <div class="d-flex flex-row align-items-start px-2">
             <div class="px-2">
               <h6 class="c-text-primary">
-                ${user.name}
+                ${userName}
                 <span style="font-size: 0.89em" class="c-text-secondary"
-                  >・ ${moment(data.createdAt).fromNow()}</span
+                  >・ ${moment(commentCreatedAt).fromNow()}</span
                 >
               </h6>
             </div>
@@ -127,12 +132,4 @@ $(document).ready(function () {
         </div>
       </div>
       `;
-        $("#comments").append(commentRow);
-      },
-      error: (error) => {
-        console.log(`error creating comment. ${error}`);
-        alert("Error making comment. Please try again in a while.");
-      },
-    });
-  });
-});
+};
